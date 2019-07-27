8391e02d0cb05701ff6ad91cd3c0e04e99269642
ski <skyjur@gmail.com>
2019-07-27 20:12:16 +0800

    feedList implementation

---
+++ src/operators/feedSortedList.test.ts
@@ -0,0 +1,126 @@
+import { deepStrictEqual } from "assert";
+import { TestScheduler } from "rxjs/testing";
+import { ChangeFeed$ } from "../types";
+import { feedSortedList, Comparator } from "./feedSortedList";
+import { map, switchMap, first, mergeMap } from "rxjs/operators";
+import { of, Subject, Unsubscribable } from "rxjs";
+
+describe("operators/feedSortedList", () => {
+  let scheduler: TestScheduler;
+
+  beforeEach(() => {
+    scheduler = new TestScheduler(deepStrictEqual);
+  });
+
+  it("update existing", () => {
+    scheduler.run(({ expectObservable }) => {
+      const input: ChangeFeed$<any> = scheduler.createColdObservable(
+        "a-b-c-|",
+        {
+          a: ["set", "1", 100],
+          b: ["set", "2", 101],
+          c: ["set", "1", 102]
+        }
+      );
+
+      const comparator: Comparator<number> = (a, b) => a - b
+
+      const output = input.pipe(
+        feedSortedList(comparator, { throttleIntervalTime: 0, scheduler }),
+        map((subjects) =>
+          subjects.map((subject) =>
+            subject.value))
+      );
+
+      expectObservable(output).toBe("a-b-c-|", {
+        a: [100],
+        b: [100, 101],
+        c: [101, 102]
+      });
+    });
+  });
+
+  it("value change that does not effect sorting does not trigger event", () => {
+    scheduler.run(({ expectObservable }) => {
+      const input: ChangeFeed$<any> = scheduler.createColdObservable(
+        "abc|",
+        {
+          a: ["set", "1", 100],
+          b: ["set", "2", 110],
+          c: ["set", "1", 101],
+        }
+      );
+
+      const output = input.pipe(
+        feedSortedList((a, b) => a - b, { throttleIntervalTime: 0, scheduler }),
+        map((subjects) =>
+          subjects.map((subject) =>
+            subject.value))
+      );
+
+      expectObservable(output).toBe("ab-|", {
+        a: [100],
+        b: [100, 110]
+      });
+    });
+  });
+
+  it("item deletion removes item from list", () => {
+    scheduler.run(({ expectObservable }) => {
+      const input: ChangeFeed$<any> = scheduler.createColdObservable(
+        "a-b-c-|",
+        {
+          a: ["set", "1", 100],
+          b: ["set", "2", 101],
+          c: ["del", "1"]
+        }
+      );
+
+      const output = input.pipe(
+        feedSortedList((a, b) => a - b, { throttleIntervalTime: 1 }),
+        map((subjects) =>
+          subjects.map((subject) =>
+            subject.value))
+      );
+
+      expectObservable(output).toBe("a-b-c-|", {
+        a: [100],
+        b: [100, 101],
+        c: [101]
+      });
+    });
+  });
+
+  it("BehaviorSubject is updated with new values and completed on deletion", () => {
+    scheduler.run(({ hot, expectObservable }) => {
+      const input: ChangeFeed$<any> = hot(
+        "a-b-c-|",
+        {
+          a: ["set", "1", 100],
+          b: ["set", "1", 101],
+          c: ["del", "1"],
+        }
+      );
+
+      const sortedList$ = input.pipe(
+        feedSortedList((a, b) => a - b, { throttleIntervalTime: 1 }),
+      );
+
+      let one$ = new Subject();
+      let oneSub: Unsubscribable
+
+      sortedList$.subscribe({
+        next(sortedList) {
+          if (!oneSub) {
+            oneSub = sortedList[0].subscribe(one$)
+          }
+        }
+      })
+
+      expectObservable(one$).toBe("a-b-|", {
+        a: 100,
+        b: 101,
+      });
+    });
+  });
+});
---
+++ src/operators/feedSortedList.ts
@@ -0,0 +1,106 @@
+import { interval, OperatorFunction, Observable, Subject, BehaviorSubject, SchedulerLike } from "rxjs";
+import { filter, map, scan, throttle, tap } from "rxjs/operators";
+import { ChangeFeed, ChangeFeed$ } from "../types";
+import { changeFeedHandler } from "../utils";
+
+export type Comparator<T> = (a: T, b: T) => number;
+
+interface Options {
+  throttleIntervalTime?: number;
+  scheduler?: SchedulerLike
+}
+
+export function feedSortedList<Value>(
+  cmp: Comparator<Value>,
+  { throttleIntervalTime = 100, scheduler }: Options = {}
+): OperatorFunction<ChangeFeed<Value, any>, Array<BehaviorSubject<Value>>> {
+  return (input: ChangeFeed$<Value>) => {
+    return new Observable(subscriber => {
+      const data = new Map<any, BehaviorSubject<Value>>();
+      let keySortIndex = new Map<any, number>();
+      let sortedKeys: any[] = [];
+      let updateIsPending = false;
+
+      const update = new Subject<void>()
+      const updateSub = update.pipe(
+        tap(() => {
+          updateIsPending = true
+        }),
+        throttle(() => interval(throttleIntervalTime, scheduler)),
+      ).subscribe({
+        next() {
+          const keys = Array.from(data.keys())
+          sortedKeys = keys.sort((key1, key2) => {
+            return cmp(
+              data.get(key1)!.value,
+              data.get(key2)!.value
+            );
+          });
+          keySortIndex = new Map(
+            sortedKeys.map((key, index) => [key, index])
+          );
+          subscriber.next(
+            sortedKeys.map((key) => data.get(key)!)
+          );
+          updateIsPending = false;
+        }
+      })
+
+      const sub = input.subscribe({
+        next: changeFeedHandler({
+          initializing() {
+            // do nothing
+          },
+          ready() {
+            // do nothing
+          },
+          set(key, newValue) {
+            if (data.has(key)) {
+              data.get(key)!.next(newValue);
+              if (!updateIsPending) {
+                const i = keySortIndex.get(key)!;
+                if (i > 0) {
+                  const valueLeft = data.get(sortedKeys[i - 1])!.value;
+                  if (cmp(valueLeft, newValue) > 0) {
+                    update.next();
+                  }
+                }
+                if (i < sortedKeys.length - 1) {
+                  const valueRight = data.get(sortedKeys[i + 1])!.value;
+                  if (cmp(newValue, valueRight) > 0) {
+                    update.next();
+                  }
+                }
+              }
+            } else {
+              data.set(key, new BehaviorSubject(newValue));
+              update.next();
+            }
+          },
+          del(key) {
+            data.get(key)!.complete();
+            data.delete(key);
+            update.next();
+          }
+        }),
+        error(e) {
+          subscriber.error(e)
+        },
+        complete() {
+          subscriber.complete();
+          for (const subject of data.values()) {
+            subject.complete();
+          }
+        }
+      });
+
+      return () => {
+        sub.unsubscribe();
+        updateSub.unsubscribe();
+        for (const subject of data.values()) {
+          subject.complete();
+        }
+      };
+    });
+  };
+}
